49060be8a57b580214bd73e539be4580efdd9035
Mottaqui Karim <taqqui.karim@gmail.com>
2019-04-04 01:28:24 -0400

    tests for random_nums.p1

--- pset_conditionals/random_nums/p1.py
+++ pset_conditionals/random_nums/p1.py
@@ -7,9 +7,7 @@ import random
 
 # generates a random number from 1 to 3
 randn = random.randint(1, 3)
-print('here', randn)
-if randn == 1:
-    print('red')
+
 # if 1, print 'red'
 # if 2, print 'green',
 # if 3, print 'blue'
