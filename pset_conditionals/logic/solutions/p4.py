61d9ac3079234c7f15bdb3b9e5fd66af7358cc7b
Mottaqui Karim <mottaquikarim@users.noreply.github.com>
2019-04-11 17:47:47 -0400

    Update p4.py

--- pset_conditionals/logic/solutions/p4.py
+++ pset_conditionals/logic/solutions/p4.py
@@ -12,31 +12,12 @@ IsEmptyString
 # determine if the str is empty or not (print True or False)
 
 
-a = ''
-b = ' '
-c = '  '
-d = '    '
-e = '        '
+my_str = '    '
 
-# for a
-if len(a) == 0:
-	print('True')
-elif a.strip():
-  print('True')
+if my_str.strip() == '':
+  print(True)
 else:
-	print('False')
-# prints True
-
-# ~~~~~~~~~~~~~~~~
-
-# for b through e
-if len(e) == 0:
-	print('True')
-elif e.strip():
-  print('True')
-else:
-	print('False')
-# prints False
+  print(False)
 
 
 # .strip() remove any spaces from a string. you do this because a string with spaces has a length and would otherwise evaluate to empty == True. But an empty string is really a falsy.
