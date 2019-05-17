3a4ecdc7024d59672507fbce701fd1fd386a998f
Julianna Garreffa <jgarreffa112@gmail.com>
2019-05-17 03:34:48 -0400

    committing new problem

---
+++ pset_basic_data_types/basics/p2.py
@@ -0,0 +1,12 @@
+"""
+Basic variables
+"""
+
+# name a variable that represents your favorite number
+
+# name another variable that represents your age
+
+# How can you access your favorite number? Print to the console
+# your favorite number WITHOUT having to actually retype that number
+
+# do the same for your age
