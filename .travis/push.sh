0a2e779d5756b12fb793b509b658463cd1858224
Mottaqui Karim <taqqui.karim@gmail.com>
2019-05-17 04:24:26 -0400

    update push

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,21 +1,46 @@
 #!/bin/sh
 
+
+set -exo pipefail
+
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 }
 
-commit_website_files() {
-  git checkout master
+commit_to_git() {
+  git init
+  git remote add origin https://${GH_TOKEN}@github.com/mottaquikarim/pydev-psets.git
+  git fetch
+  git checkout -f $TRAVIS_BRANCH 
+  git pull
+
   git add .
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [ci skip]"
 }
 
-upload_files() {
-  git remote add origin https://$GH_TOKEN@github.com/mottaquikarim/pydev-psets.git
-  git push --quiet --set-upstream origin master 
+push_to_git() {
+  git push --quiet --set-upstream origin $TRAVIS_BRANCH
 }
 
-setup_git
-commit_website_files
-upload_files
+setup_git || true
+commit_to_git || true
+push_to_git || true
+EXIT_CODE=$?
+
+exit ${EXIT_CODE}
+
+# commit_files() {
+#   git checkout master
+#   git add .
+#   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+# }
+
+# upload_files() {
+#   git remote add origin https://$GH_TOKEN@github.com/mottaquikarim/pydev-psets
+#   git push --quiet --set-upstream origin master 
+# }
+
+# setup_git
+# commit_files
+# upload_files
