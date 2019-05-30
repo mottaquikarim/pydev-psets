c46a5b043501028b5ba9d97088b109d5d46b69d5
Julianna Garreffa <jgarreffa112@gmail.com>
2019-05-30 12:40:53 -0400

    fix answer to dict counter p2

--- pset_dicts/counters_challenge/solutions/p2.py
+++ pset_dicts/counters_challenge/solutions/p2.py
@@ -8,6 +8,3 @@ from collections import Counter
 
 shopping_list = Counter(poll1) + Counter(poll2)
 # print(final_tally) # Counter({'bananas': 18, 'apples': 14, 'clementines': 8})
-
-poll1 = dict(shopping_list)
-print(poll1) # {'apples': 14, 'bananas': 18, 'clementines': 8}
